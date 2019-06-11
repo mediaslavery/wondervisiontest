6299fcce88f28608609de2c3ddab2434902cd81c
mediaslavery <43820237+mediaslavery@users.noreply.github.com>
2019-06-10 22:30:12 -0400

    Add files via upload
    massive update, almost fleshed out all pages

--- js/textscripttwo.js
+++ js/textscripttwo.js
@@ -1,11 +1,10 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["wondervision"];
-
-const typingDelay = 100;
+const textArray = ["video", "a story", "experience"];
+const typingDelay = 200;
 const erasingDelay = 100;
-const newTextDelay = 50; // Delay between current and next text edit: i changed this to startup time
+const newTextDelay = 2000; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
@@ -21,7 +20,7 @@ function type() {
   	setTimeout(erase, newTextDelay);
   }
 }
-/* I DISABLED ERASE
+
 function erase() {
 	if (charIndex > 0) {
     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
@@ -33,14 +32,10 @@ function erase() {
     cursorSpan.classList.remove("typing");
     textArrayIndex++;
     if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 10);
+    setTimeout(type, typingDelay + 1100);
   }
 }
-*/
+
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
-
-
-
-
 });
